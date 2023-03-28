a16f8a101c8731bacdb021d8e1fc7a458979f6c8
31002 <128569883+shinsdy22@users.noreply.github.com>
2023-03-28 06:13:51 +0000

    add 'or.c'

--- .ccls-cache/@home@runner@Test/and.c
+++ .ccls-cache/@home@runner@Test/and.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-main(){
-  int a=19;
-  int b=20;
-  int c=a&b;
-  printf("And연산결과:%d",c);
-}
+// main(){
+//   int a=19;
+//   int b=20;
+//   int c=a&b;
+//   printf("And연산결과:%d",c);
+// }
---
+++ or.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main() {
+  int a = 19;
+  int b = 20;
+  int c = a | b;
+  printf("Or연산결과:%d", c);
+}
