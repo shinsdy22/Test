63cd08c1d9d8cad8234a00bb5a4a07ae601369d6
shinsdy22 <128569883+shinsdy22@users.noreply.github.com>
2023-05-16 06:23:57 +0000

    switch문

---
+++ Conditio.n_three_op.c
@@ -0,0 +1,13 @@
+// #include<stdio.h>
+// main(){
+//   int y;
+//   printf("하나의 정수를 입력하세요 : ");
+//   scanf("%d",&y);
+
+//   if(y==0){
+//     printf("입력된 수는 0입니다.");
+//   }
+//   else
+//     (y>0?printf("입력된 수는 양수입니다.\n"):printf("입력된 수는 음수입니다.\n"));
+  
+// }
