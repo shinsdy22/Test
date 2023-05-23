2171f84b2fa87f8f7bc01185f1fd6be4dc24ae50
shinsdy22 <128569883+shinsdy22@users.noreply.github.com>
2023-05-23 06:11:52 +0000

    while

--- .ccls-cache/@home@runner@Test/switch.c
+++ .ccls-cache/@home@runner@Test/switch.c
@@ -1,29 +1,29 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-main() {
-  printf("사용자명을 입력해주세요 : ");
-  char name[500];
-  scanf("%s", &name);
-  printf("사과:1 키위:2 수박:3 복숭아:4 모두 좋아하지 않는다:0\n");
-  printf("%s님이 좋아하는 과일의 번호를 한 개 선택해주세요 : ", name);
+// main() {
+//   printf("사용자명을 입력해주세요 : ");
+//   char name[500];
+//   scanf("%s", &name);
+//   printf("사과:1 키위:2 수박:3 복숭아:4 모두 좋아하지 않는다:0\n");
+//   printf("%s님이 좋아하는 과일의 번호를 한 개 선택해주세요 : ", name);
 
-  int ans;
-  scanf("%d", &ans);
+//   int ans;
+//   scanf("%d", &ans);
 
-  switch(ans){
-    case 1:
-      printf("%s님은 사과를 좋아하시는군요!!", name);
-      break;
-    case 2:
-      printf("%s님은 키위를 좋아하시는군요!!", name);
-      break;
-    case 3:
-      printf("%s님은 수박를 좋아하시는군요!!", name);
-      break;
-    case 4:
-      printf("%s님은 복숭아를 좋아하시는군요!!", name);
-      break;
-    default:
-    printf("%s은 아마도 과일을 별로 좋아하지 않는 것 같군요",name);
-  }
-}
+//   switch(ans){
+//     case 1:
+//       printf("%s님은 사과를 좋아하시는군요!!", name);
+//       break;
+//     case 2:
+//       printf("%s님은 키위를 좋아하시는군요!!", name);
+//       break;
+//     case 3:
+//       printf("%s님은 수박를 좋아하시는군요!!", name);
+//       break;
+//     case 4:
+//       printf("%s님은 복숭아를 좋아하시는군요!!", name);
+//       break;
+//     default:
+//     printf("%s은 아마도 과일을 별로 좋아하지 않는 것 같군요",name);
+//   }
+// }
---
+++ while.c
@@ -0,0 +1,10 @@
+// #include<stdio.h>
+// main(){
+//   int i = 1;
+//   int sum = 0;
+//   while(i<=2019){
+//     sum+=i;
+//     i++;
+//   }
+//   printf("1-2019의 합 : %d",sum);
+// }
