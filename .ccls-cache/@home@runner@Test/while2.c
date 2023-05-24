4232c00aab50f9d1d8af23864e2e6af43ab91589
shinsdy22 <128569883+shinsdy22@users.noreply.github.com>
2023-05-24 05:06:32 +0000

    for

--- .ccls-cache/@home@runner@Test/while2.c
+++ .ccls-cache/@home@runner@Test/while2.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-main(){
-  /*int i,j;
-  printf("1-9까지 숫자를 입력하세요\n");
-  scanf("%d",&i);*/
-  int i,j;
-  i=1;
-  j=1;
-  while(j<10){
-    i=1;
-    while(i<10){
-        printf("%d * %d = %d\n",j,i,i*j);
-        i++;
-    }
-    printf("\n");
-    j++;
-  }
-}
+// #include<stdio.h>
+// main(){
+//   /*int i,j;
+//   printf("1-9까지 숫자를 입력하세요\n");
+//   scanf("%d",&i);*/
+//   int i,j;
+//   i=1;
+//   j=1;
+//   while(j<10){
+//     i=1;
+//     while(i<10){
+//         printf("%d * %d = %d\n",j,i,i*j);
+//         i++;
+//     }
+//     printf("\n");
+//     j++;
+//   }
+// }
---
+++ for_mission_1.c
@@ -0,0 +1,11 @@
+// #include<stdio.h>
+
+// main(){
+//   int i;
+//   int sum = 0;
+
+//   for(i=1;i<=2019;i++){
+//     sum+=i;
+//   }
+//   printf("1-2019까지의 합 : %d ", sum);
+// }
